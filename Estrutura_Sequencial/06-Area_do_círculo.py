6d766c16c854b49a04d5f4d30a0d87f5426ceed3
guilhermejcmarinho <gui.the.great@gmail.com>
2021-08-28 01:50:23 -0300

    Reformatação

--- Estrutura_Sequencial/06-Area_do_círculo.py
+++ Estrutura_Sequencial/06-Area_do_círculo.py
@@ -1,4 +1,5 @@
 import math
+
 raio_circulo = float(input('Informe o raio do círculo: '))
-area = (raio_circulo**2)*math.pi
-print(round (area, 2))
+area = (raio_circulo ** 2) * math.pi
+print(round(area, 2))
