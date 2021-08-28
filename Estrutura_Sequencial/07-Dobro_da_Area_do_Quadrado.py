f7e6264a89bd2c1eba424714883e6773bfe5b103
guilhermejcmarinho <gui.the.great@gmail.com>
2021-08-27 23:22:13 -0300

    Caucula o dobro da area de um quadrado.

---
+++ Estrutura_Sequencial/07-Dobro_da_Area_do_Quadrado.py
@@ -0,0 +1,4 @@
+lado_quad = int(input('Informe o tamanho do lado do quadrado: '))
+print('****************************************')
+print('O dobro da áre é: ',(lado_quad**2)*2, '!')
+print('****************************************')
