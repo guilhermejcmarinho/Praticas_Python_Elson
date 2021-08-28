6e28769d02ffa45864dbe3c5be5b5d404621006a
guilhermejcmarinho <gui.the.great@gmail.com>
2021-08-27 23:38:43 -0300

    Calcula salário por hora no mês.

---
+++ Estrutura_Sequencial/08-Salario_hora_simples.py
@@ -0,0 +1,3 @@
+sal_hora = float(input('Informe o valor da hora trabalhada: '))
+hr_trab = float(input('Informe quantas horas trabalhou: '))
+print('Salário total no mês: R$', round(sal_hora*hr_trab, 2))
