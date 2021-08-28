3dc16c217b8bea25f1ae26ef9fb842a7297c08b6
guilhermejcmarinho <gui.the.great@gmail.com>
2021-08-28 00:46:58 -0300

    Calculos de matemática simples.

---
+++ Estrutura_Sequencial/11-Cauculos_Matematicos.py
@@ -0,0 +1,13 @@
+num1 = int(input('Infomre um número inteiro: '))
+num2 = int(input('Infomre outro número inteiro: '))
+num3 = float(input('Infomre um número real: '))
+#Calculo 1 - produto do dobro do primeiro com metade do segundo.
+res1 = (2*num1)*(num2/2)
+#Calculo 2 - soma do triplo do primeiro com o terceiro.
+res2 = (3*num1)+num3
+#Calculo 3 - terceiro elevado ao cubo.
+res3 = num3**3
+#Resultados
+print('O produto do dobro do primeiro com metade do segundo é igual: \n', res1)
+print('A soma do triplo do primeiro com o terceiro é igual: \n', res2)
+print('O terceiro elevado ao cubo é igual: \n', res3)
