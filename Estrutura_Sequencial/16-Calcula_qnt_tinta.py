67ce8128788ee5a0527e33c94fb4acee62b35f97
guilhermejcmarinho <gui.the.great@gmail.com>
2021-08-31 00:05:35 -0300

    Calcula quantidade de tinta para pintar x metros quadrados.

---
+++ Estrutura_Sequencial/16-Calcula_qnt_tinta.py
@@ -0,0 +1,7 @@
+qntMetros = float(input('Informe a quantidade de m² a serem pintados:'))
+
+tintaTotal = qntMetros * 18 / 54
+#qntLatas = tintaTotal/18
+
+print('Quantidade de tinta:', round(tintaTotal), 'Litro(s).')
+#print('Quantidade de Latas:', round(qntLatas), 'Lata(s).')
