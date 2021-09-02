d3535178d5963b0e98f362ed6f9b62c6f2643c52
guilhermejcmarinho <gui.the.great@gmail.com>
2021-09-02 00:58:42 -0300

    Informa se e uma vogal ou consoante.

---
+++ 02-Estrutura_de_Decisao/04-Vogal_Consoante.py
@@ -0,0 +1,18 @@
+letra = input("Informe uma letra ou consoante:").lower()
+
+#while letra != :
+ #   print('Digite somente uma letra:')
+ #   letra = input("Informe uma letra ou consoante:").lower()
+#else:
+if letra == "a":
+    print("Vogal")
+elif letra == "e":
+    print("Vogal")
+elif letra == "i":
+    print("Vogal")
+elif letra == "o":
+    print("Vogal")
+elif letra == "u":
+    print("Vogal")
+else:
+    print("Consoante")
